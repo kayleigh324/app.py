28e0683793aea32e2111c9cbbd5bab4e327209bc
kayleigh324 <kayleightuppen@gmail.com>
2021-01-20 17:31:25 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if (request.method == 'POST'):
+        some_json = request.get_json()
+        return jsonify({'you sent': some_json}), 201
+    else:
+        return jsonify({"about": "Hello World!"})
+
+@app.route('/multi/<int:num>', methods=['GET'])
+def get_multiply10(num):
+    return jsonify({'result': num*10})
+
+languages = [{'name'  : 'JavaScript'}, {'name'  : 'Python'}, {'name'  : 'Ruby'}]   
+
+@app.route('/', methods=['GET'])
+def test():
+    return jsonify({'message': 'It works!'})
+
+@app.route('/lang', methods=['GET'])
+def returnAll():
+    return jsonify({'languages' : languages})    
+
+@app.route('/lang/<string:name>', methods=['GET'])    
+def returnOne(name):
+    langs = [language for language in languages if language['name'] == name]
+    return jsonify({'language' : langs[0]})
+
+@app.route('/lang', methods=['POST'])
+def addOne():
+    language = {'name' : request.json['name']}
+
+    languages.append(language)
+    return jsonify({'languages' : languages})
+
+@app.route('/lang/<string:name>', methods=['PUT'])
+def editOne(name):
+    langs = [language for language in languages if language['name']== name]
+    langs[0]['name'] = request.json['name']
+    return jsonify({'language' : langs[0]})
+
+@app.route('/lang/<string:name>', methods=['DELETE'])
+def removeOne(name):
+    langs = [language for language in languages if language['name']== name]
+    languages.remove(lang[0])
+    return jsonify({'languages'})
+if __name__ == '__main__' :
+    app.run(debug=True)   
+
+  
